e9100eb98f6a82b396080e787063c0ff3292b8e2
Gwilym Inzani <email@gwilym.dev>
2023-08-06 21:46:40 +0100

    Start laying out the UI

---
+++ tapir-game-engine/tapir-sounds/src/main.rs
@@ -0,0 +1,20 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use app::TapirSoundApp;
+use eframe::egui;
+
+mod app;
+mod widgets;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1024.0, 768.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Tapir Sounds",
+        options,
+        Box::new(|cc| Box::new(TapirSoundApp::new(cc))),
+    )
+}
