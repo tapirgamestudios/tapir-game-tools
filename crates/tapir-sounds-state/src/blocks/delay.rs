ac71ec9b20ba932ff82e719a678bdc51e94aad36
Gwilym Inzani <email@gwilym.dev>
2023-08-22 21:51:56 +0100

    Allow changing the input amplitude

--- crates/tapir-sounds-state/src/blocks/delay.rs
+++ crates/tapir-sounds-state/src/blocks/delay.rs
@@ -4,12 +4,16 @@ use super::{BlockCategory, BlockName, BlockType, Input};
 
 #[derive(Clone)]
 pub struct Delay {
+    input_amplitude: f64,
     delay: f64,
 }
 
 impl Default for Delay {
     fn default() -> Self {
-        Self { delay: 0.0 }
+        Self {
+            input_amplitude: 1.0,
+            delay: 0.0,
+        }
     }
 }
 
@@ -29,7 +33,7 @@ impl BlockType for Delay {
 
     fn inputs(&self) -> Rc<[(Cow<'static, str>, Input)]> {
         vec![
-            ("Input".into(), Input::Input),
+            ("Input".into(), Input::Amplitude(self.input_amplitude)),
             ("Amount".into(), Input::Periods(self.delay)),
         ]
         .into()
@@ -37,7 +41,9 @@ impl BlockType for Delay {
 
     fn set_input(&mut self, index: usize, value: &Input) {
         match (index, value) {
-            (0, Input::Input) => {}
+            (0, Input::Amplitude(input_amplitude)) => {
+                self.input_amplitude = *input_amplitude;
+            }
             (1, Input::Periods(new_delay)) => {
                 self.delay = new_delay.clamp(0.0, 10.0);
             }
@@ -52,7 +58,7 @@ impl BlockType for Delay {
 
         iter::repeat(0.0)
             .take(silence_length)
-            .chain(input.iter().copied())
+            .chain(input.iter().map(|value| *value * self.input_amplitude))
             .collect()
     }
 }
