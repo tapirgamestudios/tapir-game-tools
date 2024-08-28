b373c17e4f9f2751802b3f21a8f3b8188bd08beb
Gwilym Inzani <email@gwilym.dev>
2024-08-28 21:47:02 +0100

    Skeleton UI

--- crates/tapir-tracker/src/app.rs
+++ crates/tapir-tracker/src/app.rs
@@ -10,8 +10,57 @@ impl TapirTrackerApp {
 
 impl eframe::App for TapirTrackerApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello, world!");
+        use egui::{menu, Align, CentralPanel, Layout, SidePanel, TopBottomPanel};
+
+        TopBottomPanel::top("menu").show(ctx, |ui| {
+            menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    let _ = ui.button("New");
+                });
+
+                ui.menu_button("Help", |ui| {
+                    let _ = ui.button("About");
+                })
+            });
+        });
+
+        SidePanel::left("controls_and_sections").show(ctx, |ui| {
+            ui.label("Play, pause etc");
+            ui.label("All the parts of your track");
+        });
+
+        TopBottomPanel::top("instruments").show(ctx, |ui| {
+            ui.with_layout(
+                Layout::with_main_justify(Layout::left_to_right(Align::LEFT), true),
+                |ui| {
+                    ui.vertical(|ui| {
+                        ui.horizontal(|ui| {
+                            ui.label("Instrument 1");
+                            ui.label("Instrument 2");
+                        });
+
+                        ui.horizontal(|ui| {
+                            ui.label("Channel 1");
+                            ui.label("Channel 2");
+                            ui.label("Channel 3");
+                            ui.label("Channel 4");
+                            ui.label("Channel 5");
+                        });
+                    });
+
+                    ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
+                        let _ = ui.button("Settings");
+                    });
+                },
+            );
+        });
+
+        TopBottomPanel::bottom("spectrum").show(ctx, |ui| {
+            ui.label("Resulting channel display");
+        });
+
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Piano roll goes here");
         });
     }
 }
--- crates/tapir-tracker/src/main.rs
+++ crates/tapir-tracker/src/main.rs
@@ -4,17 +4,26 @@
 use std::env;
 
 use app::TapirTrackerApp;
+use egui::ViewportBuilder;
 
 mod app;
+mod widgets;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions::default();
+
+    let options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default()
+            .with_maximized(true)
+            .with_min_inner_size((1200f32, 800f32))
+            .with_title("Tapir Tracker"),
+        ..Default::default()
+    };
 
     let args: Vec<_> = env::args().collect();
 
     eframe::run_native(
-        "Tapir Tracker",
+        "tapir_tracker",
         options,
         Box::new(move |cc| Ok(Box::new(TapirTrackerApp::new(cc, args.get(1).cloned())))),
     )
